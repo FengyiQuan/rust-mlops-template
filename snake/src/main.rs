94d055432fcf13399f4ca6b49333cab2b36d0e6b
FengyiQuan <quan.f@husky.neu.edu>
2023-03-20 23:01:34 -0400

    snake

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,8 +1,10 @@
-mod snake_game;
+mod snake_model;
+// mod snake_controller;
 
-// use snake_game::SnakeGame;
+use snake_model::*;
+// use snake_controller::*;
+
 use bevy::prelude::*;
-// use snakeView::SnakeView;
 pub static BLOCK_WIDTH: u32 = 64;
 pub static BLOCK_HEIGHT: u32 = 48;
 pub static BLOCK_SIZE: u32 = 10;
@@ -10,14 +12,7 @@ pub static BLOCK_SIZE: u32 = 10;
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 const FOOD_COLOR: Color = Color::rgb(1.0, 0.0, 1.0);
 const SNAKE_SEGMENT_COLOR: Color = Color::rgb(0.3, 0.3, 0.3);
-// Controller
-// struct Controller {
-//     game: SnakeGame,
-//     // view: SnakeView,
-// }
 
-#[derive(Component)]
-struct SnakeHead;
 #[derive(Component, Clone, Copy, PartialEq, Eq)]
 struct Position {
     x: i32,
@@ -51,50 +46,41 @@ fn spawn_snake(mut commands: Commands) {
             // },
             ..default()
         })
-        .insert(SnakeHead)
+        .insert(snake_model::SnakeHead)
         .insert(Position { x: 3, y: 3 }) // <--
         .insert(Size::square(0.8)); // <--;
 }
-
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>
+    mut head_positions: Query<&mut Position, With<SnakeHead>>
 ) {
-    for mut transform in head_positions.iter_mut() {
+    for mut pos in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.0;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.0;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.0;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.0;
+            pos.y += 1;
         }
     }
 }
 
-// pub struct View {
-//     snakeGame: SnakeGame, // Rotation for the square.
-// }
-
-// impl View {
-//     fn spawn_snake(mut commands: Commands) {}
-// }
-
 fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
     fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
         let tile_size = bound_window / bound_game;
-        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+        (pos / bound_game) * bound_window - bound_window / 2.0 + tile_size / 2.0
     }
     let window = windows.get_primary().unwrap();
     for (pos, mut transform) in q.iter_mut() {
         transform.translation = Vec3::new(
             convert(pos.x as f32, window.width() as f32, BLOCK_WIDTH as f32),
             convert(pos.y as f32, window.height() as f32, BLOCK_HEIGHT as f32),
-            0.0,
+            0.0
         );
     }
 }
@@ -102,14 +88,21 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
     let window = windows.get_primary().unwrap();
     for (sprite_size, mut transform) in q.iter_mut() {
         transform.scale = Vec3::new(
-            sprite_size.width / BLOCK_WIDTH as f32 * window.width() as f32,
-            sprite_size.height / BLOCK_HEIGHT as f32 * window.height() as f32,
-            1.0,
+            (sprite_size.width / (BLOCK_WIDTH as f32)) * (window.width() as f32),
+            (sprite_size.height / (BLOCK_HEIGHT as f32)) * (window.height() as f32),
+            1.0
         );
     }
 }
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Snake!".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..default()
+        })
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
         .add_system(snake_movement)
