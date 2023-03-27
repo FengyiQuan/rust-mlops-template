a50a5feab79463db8340bbe790d885f23892dc7c
FengyiQuan <quan.f@husky.neu.edu>
2023-03-26 23:44:50 -0400

    add

--- snake/src/snake_model.rs
+++ snake/src/snake_model.rs
@@ -1,5 +1,72 @@
 use bevy::prelude::*;
+use std::time::Duration;
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
 
-#[derive(Component)]
-pub struct SnakeHead;
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 
+
+pub struct SnakeHead {
+    pub direction: Direction,
+}
+
+pub struct Materials {
+    pub head_material: Handle<ColorMaterial>,
+    pub segment_material: Handle<ColorMaterial>,
+    pub food_material: Handle<ColorMaterial>,
+}
+
+pub struct SnakeMoveTimer(Timer);
+
+pub struct GameOverEvent;
+pub struct GrowthEvent;
+
+#[derive(Default)]
+pub struct LastTailPosition(Option<Position>);
+
+pub struct SnakeSegment;
+#[derive(Default)]
+pub struct SnakeSegments(Vec<Entity>);
+
+
+pub struct Food;
+
+pub struct FoodSpawnTimer(Timer);
+impl Default for FoodSpawnTimer {
+    fn default() -> Self {
+        Self(Timer::new(Duration::from_millis(1000), true))
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+impl Direction {
+    pub fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+        }
+    }
+}
